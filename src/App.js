09e2965537991c38a1a27c8b57f2dc4e6c43dbcf
Justin Byrd <justinbyrd7@gmail.com>
2023-08-17 16:23:13 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border p-1 m-2 shadow-md w-3/4 rounded">
+      <h1>Form</h1>
     </div>
   );
 }
-
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      custBlue: "E083D77",
+      custBeige: "#EBEBD3",
+      custRed: "#DA4167",
+      custYellow: "#F4D35E",
+      custCoral: "#F78764",
+    },
+  },
+  plugins: [],
+};
